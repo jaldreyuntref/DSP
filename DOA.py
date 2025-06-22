9abc9f6cc714597a8806bc770c98df0dd3bc8c6e
juanaldrey-untref <aldrey62235@estudiantes.untref.edu.ar>
2025-06-22 19:43:46 -0300

    se movieron archivos

--- DOA.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def doa(delays, distance):
-    angles = 0
-    for delay in delays:
-        angles += np.degrees(np.arccos((delay * 343) / (distance )))
-        
-    return angles / len(delays)
